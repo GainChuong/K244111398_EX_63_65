678037d0a12d8fb56a33152d6196112a20ae0991
Giang Nguyễn <gameyuno123@gmail.com>
2026-06-12 14:08:37 +0700

    Add files via upload

---
+++ EX63/js/dataset.js
@@ -0,0 +1,6 @@
+emps=[
+    {"Name":"John","Email":"john@gmail.com","Gender":"Man","Birthday":"02/02/1990","Hobbies":"Reading","Colors":"Yellow"},
+    {"Name":"Peter","Email":"peter@gmail.com","Gender":"Man","Birthday":"01/01/1992","Hobbies":"chat,reading","Colors":"Red"},
+    {"Name":"Lucy","Email":"lucy@gmail.com","Gender":"Woman","Birthday":"01/02/2005","Hobbies":"Listening,Chat","Colors":"Violet"},
+
+]
